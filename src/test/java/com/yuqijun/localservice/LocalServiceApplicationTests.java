15c0477e9444f53eeefcc469ea4920e5a2b9b487
yuqijun <yuqijun@nihaosi.com>
2021-07-18 14:05:15 +0800

    第一次提交本地服务

---
+++ src/test/java/com/yuqijun/localservice/LocalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuqijun.localservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
